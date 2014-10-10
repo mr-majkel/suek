1272dc5ae69b75b40aa30cd50515065a1c00213e
Michal Modzelewski <mr-majkel@users.noreply.github.com>
2014-10-10 15:30:41 +0200

    getCorrect zmiana kodowania na UTF-8 i dodanie komentarza

--- testy osiagniec/TOS 6/mat/skrypty/getCorrect.R
+++ testy osiagniec/TOS 6/mat/skrypty/getCorrect.R
@@ -1,3 +1,5 @@
+# Funkcja pomocnicza do wskazywania poprawnej odpowiedzi przy wczytywaniu
+# codebooka. Nieskończona. Obecnie nie używana.
 getCorrect = function(cdbook, mcq, char = "*") {
     # wybranie zmiennych z codebooka
     cdbook2 = cdbook[, grep("Nazwa|Etykiety", names(cdbook))]
@@ -5,14 +7,14 @@ getCorrect = function(cdbook, mcq, char = "*") {
     # zmiana nazw zmiennych na bardziej przyjazne
     names(cdbook2) = c("var_id", "values")
     
-    # etykiety dla zamkni�tych
+    # etykiety dla zamkniętych
     mcq_val = cdbook[cdbook2$var_id %in% mcq, "values"]
     names(mcq_val) = mcq
     
     # data.frame z poprawnymi odpowiedziami
     correct_df = data.frame()
     
-    # poszukiwanie specjalnego znaku dla ka�dego zadania
+    # poszukiwanie specjalnego znaku dla każdego zadania
     for (m in mcq) {
         # rozbicie etykiet
         mcq_val_list = lapply(mcq_val, strplit, "\n")
