3296343b69184de66c918f8c4fb6866e13c18df3
Michal Modzelewski <mr-majkel@users.noreply.github.com>
2014-12-05 10:15:07 +0100

    przerzuca początkowy data munging do oddzielnego skryptu

--- testy osiagniec/TOS 6/mat/skrypty/skalowanie.R
+++ testy osiagniec/TOS 6/mat/skrypty/skalowanie.R
@@ -6,49 +6,11 @@ wd = getwd()
 options("stringsAsFactors" = FALSE)
 
 library(TAM)
-library(sirt)
-library(mirt)
-library(WrightMap)
-library(xtable)
 library(plyr)
 
 source("skrypty\\pomocnicze\\difWang.R")
-source("skrypty\\pomocnicze\\anchor.R")
-
 # wczytanie baz
-mat_all = read.csv2("bazy zmien\\tos6_mat.csv")
-mat_all_r = read.csv2("bazy zmien\\tos6_mat_rek.csv")
-
-ucz_df = read.csv2("bazy oryg\\uczen_dane.csv")
-
-# dodanie płci, id szkoły, oznaczenia oddziału w klasie VI i nru z dziennika
-mat_all = merge(mat_all, ucz_df, all.x = TRUE)
-mat_all_r = merge(mat_all_r, ucz_df, all.x = TRUE)
-
-# określenie kotwiczących
-kotw = matrix(c("MA_4", "MB_4",
-                "MA_6", "MB_6",
-                "MA_8", "MB_7",
-                "MA_9", "MB_8",
-                "MA_10", "MB_9",
-                "MA_12", "MB_12",
-                "MA_13", "MB_13",
-                "MA_14", "MB_14",
-                "MA_15", "MB_16",
-                "MA_16", "MB_17",
-                "MA_18", "MB_18",
-                "MA_22", "MB_22",
-                "MA_23", "MB_23",
-                "MA_24", "MB_24"), ncol = 2, byrow = TRUE)
-
-colnames(kotw) = c("wersja A", "wersja B")
-# stworzenie zmiennych kotwiczących w bazach
-mat_all[, paste0("K", kotw[, 1])] = NA
-mat_all_r[, paste0("K", kotw[, 1])] = NA
-
-# skopiowanie wartości dla zmiennych kotwiczących
-mat_all = anchor(mat_all, kotw)
-mat_all_r = anchor(mat_all_r, kotw)
+load("bazy zmien/bazy_mat.RData")
 
 # nazwy wszystkich itemów w bazie
 items_all = grep("MA|MB", names(mat_all_r), value = TRUE)
@@ -228,10 +190,7 @@ for (it in items_kotw) {
 ### Sprawdzenie inwariantności na płeć #######
 ###### DIF na płeć ###########################
 ##############################################
-# Stworzenie zmiennej 0-1 dla płci
-mat_all_r$kobieta = NA
-mat_all_r[mat_all_r$plec == "K", "kobieta"] = 1
-mat_all_r[mat_all_r$plec == "M", "kobieta"] = 0
+
 
 ### wielo aspektowy Rasch ####################
 # model w pełni wariantny, różne średnie
